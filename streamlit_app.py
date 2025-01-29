0baf57682d8073d60561a61fe440f2a2868288be
Samhitasur <123887731+Samhitasur@users.noreply.github.com>
2025-01-29 14:45:09 +0000

    Update streamlit_app.py
    json df

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,6 +42,7 @@ if time_to_insert and ingredients_list:
 
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+#st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width = True)
 
         
