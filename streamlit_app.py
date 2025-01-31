9099e4b6e3edaef70296ce17e00a71d4ecf30975
Samhitasur <123887731+Samhitasur@users.noreply.github.com>
2025-01-31 09:35:12 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,16 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
-import pandas
+
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write(
-    """Choose the fruits you want in your custom Smoothie!"""
+    """Choose the fruits you want in your custom Smoothie!
+    """
 )
 
-name_on_order = st.text_input('Name on Smoothie:')
+name_on_order = st.text_input('Name on Smoothie :')
 st.write('The name on your Smoothie will be:', name_on_order)
 
 cnx = st.connection("snowflake")
@@ -38,16 +39,16 @@ if ingredients_list:
         ingredients_string += fruit_chosen + ' '
 
         search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
+        #st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
         
         st.subheader(fruit_chosen + ' Nutrition Information')
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
-        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
+        sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width = True)
 
-    st.write(ingredients_string)
+    #st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
-            values ('""" + ingredients_string + """', '""" + name_on_order + """')"""
+                values ('""" + ingredients_string + """', '"""+name_on_order+ """')"""
 
     #st.write(my_insert_stmt)
     #st.stop()
@@ -57,5 +58,5 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
 
-        st.success('Your Smoothie is ordered, '+ name_on_order + '!', icon="✅")
+        st.success('Your Smoothie is ordered, ' + name_on_order, icon="✅")
         
